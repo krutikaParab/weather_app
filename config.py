e1970af331e736224684c316dbe280110d6a549a
krutikaparab14 <kdparab001@gmail.com>
2025-12-28 00:08:26 +0530

    Adding module docstring

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-# config.py
+"""
+Configuration file for weather application.
+Contains API keys and endpoint URLs."""
 API_KEY = "your_actual_api_key"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
